f1d8b6ecdce23a52833000538ec75dd3eebd6887
vikash-vishwakarma <kvikash1495@gmail.com>
2019-10-23 14:04:02 +0530

    modified code

--- src/main/java/com/example/demo/ParentApplication.java
+++ src/main/java/com/example/demo/ParentApplication.java
@@ -8,7 +8,6 @@ public class ParentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParentApplication.class, args);
-		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<booted>>>>>>>>>>>>>>>>>>>>>>>");
 }
 
 
